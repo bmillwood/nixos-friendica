d98a34c6507d982364d18ff8e9259362c3f2c2b5
Ben Millwood <thebenmachine+git@gmail.com>
2024-10-18 20:08:45 +0100

    steal more phpfpm settings

--- default.nix
+++ default.nix
@@ -169,9 +169,13 @@ in
         group = cfg.group;
         settings = {
           "listen.owner" = config.services.httpd.user;
-          "pm" = "static";
-          "pm.max_children" = 32;
           # copied from https://wiki.nixos.org/wiki/Phpfpm
+          "pm" = "dynamic";
+          "pm.max_children" = 32;
+          "pm.max_requests" = 500;
+          "pm.start_servers" = 2;
+          "pm.min_spare_servers" = 2;
+          "pm.max_spare_servers" = 5;
           "php_admin_value[error_log]" = "stderr";
           "php_admin_flag[log_errors]" = true;
           "catch_workers_output" = true;
